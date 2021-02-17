4f1e99fa7847b1274e088015dcb861eefa77c5e9
Todd Albert <todd@bocacode.com>
2021-02-17 16:00:50 -0500

    added react-bootstrap and getting activities from API

---
+++ src/scenes/Activitities.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react'
+import Spinner from 'react-bootstrap/Spinner'
+import ListGroup from 'react-bootstrap/ListGroup'
+
+function Activities() {
+  const [activitiesList, setActivitiesList] = useState(null)
+  useEffect(() => {
+    fetch('https://tracker-bocacode.web.app/activities')
+      .then(res => res.json())
+      .then(data => setActivitiesList(data))
+      .catch(error => alert('Error getting Activities!'))
+  }, [])
+  if (!activitiesList) {
+    return (
+      <Spinner animation="border" role="status">
+        <span className="sr-only">Loading...</span>
+      </Spinner>
+    )
+  }
+  return (
+    <ListGroup>
+      {activitiesList.map(activity => {
+        return <ListGroup.Item key={activity.id}>{activity.name}</ListGroup.Item>
+      })}
+    </ListGroup>
+  )
+}
+
+export default Activities
